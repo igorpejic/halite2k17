dbd4760df29062d19df687c042df1ba4582094cb
Igor Pejic <igor.pejic@dr.com>
2017-11-11 15:02:41 +0100

    always attack closest planet

--- MyBot.py
+++ MyBot.py
@@ -28,50 +28,29 @@ def get_ship_command(ship):
 
     # For each planet in the game (only non-destroyed planets are included)
     entities_by_distance = game_map.nearby_entities_by_distance(ship)
-    planets = [planet for distance in entities_by_distance for planet in entities_by_distance[distance]]
+    planets = [planet for distance in entities_by_distance for planet in entities_by_distance[distance] if isinstance(planet, hlt.entity.Planet)]
     non_taken_planets = [p for p in planets if isinstance(p, hlt.entity.Planet) and not p.is_owned()]
     taken_planets = [p for p in planets if isinstance(p, hlt.entity.Planet) and p.is_owned()]
-    if non_taken_planets:
-        for non_taken_planet in non_taken_planets:
-        # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
-            if ship.can_dock(non_taken_planet):
-                return ship.dock(non_taken_planet)
-                break
-            else:
-                # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
-                # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
-                # We run this navigate command each turn until we arrive to get the latest move.
-                # Here we move at half our maximum speed to better control the ships
-                # In order to execute faster we also choose to ignore ship collision calculations during navigation.
-                # This will mean that you have a higher probability of crashing into ships, but it also means you will
-                # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
-                # wish to turn that option off.
-                return ship.navigate(
-                    ship.closest_point_to(non_taken_planet),
-                    game_map,
-                    speed=int(hlt.constants.MAX_SPEED),
-                    ignore_ships=True)
-    else:
-        for non_taken_planet in taken_planets:
-        # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
-            if ship.can_dock(non_taken_planet):
-                return ship.dock(non_taken_planet)
-            else:
-
-                # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
-                # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
-                # We run this navigate command each turn until we arrive to get the latest move.
-                # Here we move at half our maximum speed to better control the ships
-                # In order to execute faster we also choose to ignore ship collision calculations during navigation.
-                # This will mean that you have a higher probability of crashing into ships, but it also means you will
-                # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
-                # wish to turn that option off.
-                navigate_command = ship.navigate(
-                    ship.closest_point_to(non_taken_planet),
-                    game_map,
-                    speed=int(hlt.constants.MAX_SPEED/2),
-                    ignore_ships=True)
-                return navigate_command
+    logging.info(planets)
+    for non_taken_planet in planets:
+    # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
+        if ship.can_dock(non_taken_planet):
+            return ship.dock(non_taken_planet)
+            break
+        else:
+            # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
+            # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
+            # We run this navigate command each turn until we arrive to get the latest move.
+            # Here we move at half our maximum speed to better control the ships
+            # In order to execute faster we also choose to ignore ship collision calculations during navigation.
+            # This will mean that you have a higher probability of crashing into ships, but it also means you will
+            # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
+            # wish to turn that option off.
+            return ship.navigate(
+                ship.closest_point_to(non_taken_planet),
+                game_map,
+                speed=int(hlt.constants.MAX_SPEED),
+                ignore_ships=True)
 while True:
     # TURN START
     # Update the map for the new turn and get the latest version
