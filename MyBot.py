380a57929d84bf63697cfad49cf1f03717f8f457
Igor Pejic <igor.pejic@dr.com>
2017-11-11 16:19:22 +0100

    add faster expansion; timing out still

--- MyBot.py
+++ MyBot.py
@@ -1,15 +1,3 @@
-"""
-Welcome to your first Halite-II bot!
-
-This bot's name is Settler. It's purpose is simple (don't expect it to win complex games :) ):
-1. Initialize game
-2. If a ship is not docked and there are unowned planets
-2.a. Try to Dock in the planet if close enough
-2.b If not, go towards the planet
-
-Note: Please do not place print statements here as they are used to communicate with the Halite engine. If you need
-to log anything use the logging module.
-"""
 # Let's start by importing the Halite Starter Kit so we can interface with the Halite engine
 import hlt
 # Then let's import the logging module so we can print out information
@@ -21,36 +9,46 @@ game = hlt.Game("Settler")
 # Then we print our start message to the logs
 logging.info("Starting my Settler bot!")
 
-def get_ship_command(ship):
-    if ship.docking_status != ship.DockingStatus.UNDOCKED:
-        # Skip this ship
-        return None
+
+def get_ship_command(ship, ships_len):
+    if ships_len < 20:
+        if ship.docking_status != ship.DockingStatus.UNDOCKED:
+            # Skip this ship
+            return None
 
     # For each planet in the game (only non-destroyed planets are included)
     entities_by_distance = game_map.nearby_entities_by_distance(ship)
-    planets = [planet for distance in entities_by_distance for planet in entities_by_distance[distance] if isinstance(planet, hlt.entity.Planet)]
-    non_taken_planets = [p for p in planets if isinstance(p, hlt.entity.Planet) and not p.is_owned()]
-    taken_planets = [p for p in planets if isinstance(p, hlt.entity.Planet) and p.is_owned()]
-    logging.info(planets)
-    for non_taken_planet in planets:
-    # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
+    logging.info(entities_by_distance)
+    sorted_list = sorted(
+        entities_by_distance.items(), key=lambda value: value[0])
+    planets = []
+    for element in sorted_list:
+        for el in element[1]:
+            if isinstance(el, hlt.entity.Planet):
+                planets.append(el)
+    if planets:
+        try:
+            non_taken_planet = next(
+                planet for planet in planets if not planet.is_owned())
+        except:
+            # no more non_taken_planet
+            non_taken_planet = next(
+                planet for planet in planets)
+    else:
+        return None
+    logging.info(non_taken_planet)
+    if non_taken_planet:
         if ship.can_dock(non_taken_planet):
             return ship.dock(non_taken_planet)
-            break
         else:
-            # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
-            # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
-            # We run this navigate command each turn until we arrive to get the latest move.
-            # Here we move at half our maximum speed to better control the ships
-            # In order to execute faster we also choose to ignore ship collision calculations during navigation.
-            # This will mean that you have a higher probability of crashing into ships, but it also means you will
-            # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
-            # wish to turn that option off.
             return ship.navigate(
                 ship.closest_point_to(non_taken_planet),
                 game_map,
                 speed=int(hlt.constants.MAX_SPEED),
                 ignore_ships=True)
+    else:
+        return None
+
 while True:
     # TURN START
     # Update the map for the new turn and get the latest version
@@ -59,9 +57,10 @@ while True:
     # Here we define the set of commands to be sent to the Halite engine at the end of the turn
     command_queue = []
     # For every ship that I control
-    for ship in game_map.get_me().all_ships():
+    total_ships = game_map.get_me().all_ships()
+    for ship in total_ships:
         # If the ship is docked
-        ship_command = get_ship_command(ship)
+        ship_command = get_ship_command(ship, len(total_ships))
         if ship_command:
             command_queue.append(ship_command)
             # Send our set of commands to the Halite engine for this turn
